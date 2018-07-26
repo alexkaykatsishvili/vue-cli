da74cae3b60fc47957f7c55a7f4eef5aecff38fa
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-07-26 15:29:29 +0300

    Work with components: Greetings and Time

---
+++ src/Greetings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="greetings">
+    <h1>Hello, {{name}}!</h1>
+    <button @click="changeName">Not {{name}}?</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        name: 'Alex'
+      }
+    },
+    methods: {
+      changeName () {
+        this.name = this.name == 'Alex' ? 'Daniel' : 'Alex';
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/Time.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="time">
+    <h3>What time is it?</h3>
+    <h4>Just push a button below!</h4>
+    <button type="button" name="button" @click="showCurrentTime">Show</button>
+    <p>Your current time is {{time}}</p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        time: ''
+      }
+    },
+    methods: {
+      showCurrentTime () {
+        var hms = new Date();
+        this.time = hms;
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .time {
+    color: #ccc;
+    p {
+      color: black;
+    }
+  }
+</style>
